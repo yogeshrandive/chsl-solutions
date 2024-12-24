79b08689508f6f76b26fc07d0d3a4ea9e1a62e91
Yogesh Randive <109646848+yogeshmavonic@users.noreply.github.com>
2024-12-24 23:17:56 +0530

    update the name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 // import { MainSidebar } from '@/components/main-sidebar';
 // import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
-import { getUserDetails } from '@/lib/dal';
-import { Toaster } from '@/components/ui/toaster';
+import { getUserDetails } from "@/lib/dal";
+import { Toaster } from "@/components/ui/toaster";
 // import { HeaderContent } from '@/components/header-content';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Society ManagementApp',
-  description: 'A modern application with shadcn/ui',
+  title: "Society Management App",
+  description: "A modern application with shadcn/ui",
 };
 
 export default async function RootLayout({
@@ -25,13 +25,13 @@ export default async function RootLayout({
     <html lang="en">
       <body className={`${inter.className} flex h-full`}>
         <Toaster />
-        {userData != null ? (
-          <div className="flex flex-col flex-1  w-full">
-            <main>{children}</main>
-          </div>
-        ) : (
-          <div className="flex flex-col min-h-screen w-full">{children}</div>
-        )}
+        {userData != null
+          ? (
+            <div className="flex flex-col flex-1  w-full">
+              <main>{children}</main>
+            </div>
+          )
+          : <div className="flex flex-col min-h-screen w-full">{children}</div>}
       </body>
     </html>
   );
